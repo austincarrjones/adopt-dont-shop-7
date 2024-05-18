617d93b3101272814332a7933f1345f3a6114fe1
Austin Carr-Jones <austincarr.jones@gmail.com>
2024-05-18 17:51:04 -0600

    progress a little for AK to inspect

--- spec/features/pet_applications/show_spec.rb
+++ spec/features/pet_applications/show_spec.rb
@@ -26,5 +26,17 @@ RSpec.describe "the application show" do
         expect(page).to have_content("Address: 5 Haytown Rd., Lebanon, NJ, 08889")
     end
 
-    xit ""
+    it "shows names of all pets this application is for (and links to their show page)" do
+        cesarsapp = PetApplication.create!(name: "Cesar Milan", street: "5 Haytown Rd.", city: "Lebanon", state: "NJ", zip: "08889", description: "I'd be great")
+        shelter = Shelter.create(name: "Mystery Building", city: "Irvine CA", foster_program: false, rank: 9)
+        scooby = Pet.create(name: "Scooby", age: 2, breed: "Great Dane", adoptable: true, shelter_id: shelter.id)
+        PetApplicationPet.create!(pet_application: cesarsapp, pet: scooby)
+        binding.pry
+        visit "/pet_applications/#{cesarsapp.id}"
+        expect(page).to have_content(scooby.name)
+        # expect(page).to have_link("Scooby", href: "/pets/#{scooby.id}") #not sure which test is best
+        click_link("#{scooby}")
+        expect(current_path).to eq("/pets/#{scooby.id}")
+    end
+
 end
