2b32e7f721c0714138af05712ee2389ea12cc6bf
Austin Carr-Jones <austincarr.jones@gmail.com>
2024-05-18 16:06:43 -0600

    test: pass model test pet_application_pet

--- app/models/pet_application_pet.rb
+++ app/models/pet_application_pet.rb
@@ -1,3 +1,4 @@
 class PetApplicationPet < ApplicationRecord
-  
+  belongs_to :pet
+  belongs_to :pet_application
 end
--- spec/models/pet_application_pet_spec.rb
+++ spec/models/pet_application_pet_spec.rb
@@ -1,6 +1,6 @@
 require "rails_helper"
 
-RSpec.describe PetApplicationPets, type: :model do
+RSpec.describe PetApplicationPet, type: :model do
   describe "relationships" do
     it {should belong_to :pet}
     it {should belong_to :pet_application}
