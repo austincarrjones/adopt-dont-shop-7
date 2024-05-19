ae611ceb51925731c73b48c9cc7df96350a8757a
Austin K <austink@Austins-MacBook-Pro.local>
2024-05-19 18:00:15 -0400

    US2 and tests

--- app/controllers/pet_applications_controller.rb
+++ app/controllers/pet_applications_controller.rb
@@ -1,5 +1,13 @@
 class PetApplicationsController < ApplicationController
+  def new
+  end
 
+  def create
+    @application = PetApplication.create(name: params[:name], street: params[:street], city: params[:city], 
+    state: params[:state], zip: params[:zip], description: params[:description])
+    redirect_to "/pet_applications/#{@application.id}"
+
+  end
   def show
     # binding.pry
     @pet_application = PetApplication.find(params[:id])
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,35 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+application = PetApplication.create!(
+                                    name: "Cesar Milan",
+                                    street: "5 Haytown Rd.",
+                                    city: "Lebanon",
+                                    state: "NJ",
+                                    zip: "08889",
+                                    description: "I'd be great",
+                                    status: "In Progress"
+)
+shelter = Shelter.create!(
+                        name: "Mystery Building",
+                        city: "Irvine CA",
+                        foster_program: false,
+                        rank: 9
+)
+pet1 = Pet.create!(
+                name: "Scooby",
+                age: 2,
+                breed: "Great Dane",
+                adoptable: true,
+                shelter_id: shelter.id
+)
+pet2 = Pet.create(
+                adoptable: true,
+                age: 3,
+                breed: "doberman",
+                name: "Lobster",
+                shelter_id: shelter.id
+)
+PetApplicationPet.create!(pet_application: application, pet: pet1)
+PetApplicationPet.create!(pet_application: application, pet: pet2)
